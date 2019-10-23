c0b8f2f95859ed8bbad88312e41f77f8846e6719
DeathWish99 <DeathWish99@users.noreply.github.com>
2019-10-23 22:48:37 +0700

    Initial commit

---
+++ Assets/GenerateTerrain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour
+{
+    [SerializeField] private Transform levelPart_1;
+    private void Awake()
+    {
+        Instantiate(levelPart_1, new Vector3(29.92f, -2.4f, 0), Quaternion.identity);
+    }
+
+}
