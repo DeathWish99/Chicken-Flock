0bee141bbf5bb26fa45090023090fdbea3ff577d
DeathWish99 <44440688+DeathWish99@users.noreply.github.com>
2019-10-24 11:07:34 +0700

    pUSH

--- Assets/GenerateTerrain.cs
+++ Assets/GenerateTerrain.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class GenerateTerrain : MonoBehaviour
 {
+    [SerializeField] private Transform levelPart_start;
     [SerializeField] private Transform levelPart_1;
+
+    private Vector3 lastEndPosition;
+
+    private IEnumerator coroutine;
     private void Awake()
     {
-        Instantiate(levelPart_1, new Vector3(29.92f, -2.4f, 0), Quaternion.identity);
+        lastEndPosition = levelPart_start.Find("EndPoint").position;
+        SpawnLevelPart();
     }
 
+    private void Update()
+    {
+        coroutine = Generate(3f);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator Generate(float waitTime)
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            SpawnLevelPart();
+        }
+    }
+    private void SpawnLevelPart()
+    {
+        Transform lastLevelPartTransform = SpawnLevelPartPosition(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPoint").position;
+    }
+
+    private Transform SpawnLevelPartPosition(Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
+    }
 }
