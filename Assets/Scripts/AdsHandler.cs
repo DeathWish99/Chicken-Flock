8b390480d33eec84860117ec96826e29ca171f90
zalfornt <44518346+zalfornt@users.noreply.github.com>
2020-01-24 17:54:38 +0700

    First Release

---
+++ Assets/Scripts/AdsHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsHandler : MonoBehaviour
+{
+
+    string gameId;
+    bool testMode = false;
+
+    //string rewardedAds = "rewardedVideo";
+    string videoAds = "video";
+
+    public static AdsHandler Instance { get; set; }
+
+    void Awake()
+    {
+        if (Application.isEditor) testMode = true;
+#if UNITY_ANDROID
+        gameId = "3431809";
+#elif UNITY_IOS
+        gameId = "3431808";
+#endif
+        Advertisement.Initialize(gameId, testMode);
+        Debug.Log("test mode: " + testMode);
+
+        Instance = this;
+    }
+
+    public void ShowVideoAds()
+    {
+        if (Advertisement.IsReady(videoAds))
+        {
+            Advertisement.Show(videoAds);
+        }
+    }
+
+}
--- Assets/Scripts/Ui/MainMenuButton.cs
+++ Assets/Scripts/Ui/MainMenuButton.cs
@@ -1,22 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Advertisements;
-
+using UnityEngine.SceneManagement;
 
 public class MainMenuButton : MonoBehaviour, IPointerClickHandler
 {
+
     public void OnPointerClick(PointerEventData eventData)
     {
         SceneManager.LoadScene(0);
 
         PlayerPrefs.SetInt("Retry", 0);
 
-        if (Advertisement.IsReady())
-        {
-            Advertisement.Show();
-        }
+        AdsHandler.Instance.ShowVideoAds();
     }
 }
